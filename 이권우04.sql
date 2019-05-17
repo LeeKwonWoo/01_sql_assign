4590c8a54b4e8a2a95eaa3ef8cf09051e02e4cc5
LeeKwonWoo <kenu12@Naver.com>
2019-05-17 16:15:27 +0900

    과제 04

---
+++ 이권우04.sql
@@ -0,0 +1,92 @@
+-- 실습 01)
+SELECT INITCAP(e.ename)
+  FROM emp e
+;
+
+/*
+INITCAP(E.ENAME)
+----------------
+Smith
+Allen
+Ward
+Jones
+Martin
+Blake
+Clark
+King
+Turner
+James
+Ford
+Miller
+J_James
+J%James
+*/
+
+-- 실습 2)
+SELECT LOWER(e.ename)
+  FROM emp e
+;
+
+/*
+LOWER(E.ENAME)
+--------------
+smith
+allen
+ward
+jones
+martin
+blake
+clark
+king
+turner
+james
+ford
+miller
+j_james
+j%james
+*/
+
+
+-- 실습 3)
+SELECT INITCAP(e.ename)
+  FROM emp e
+;
+
+/*
+INITCAP(E.ENAME)
+----------------
+Smith
+Allen
+Ward
+Jones
+Martin
+Blake
+Clark
+King
+Turner
+James
+Ford
+Miller
+J_James
+J%James
+*/
+
+
+--실습 4)
+SELECT LENGTH('korea')
+  FROM dual
+;
+SELECT LENGTH('korea')
+  FROM dual
+;
+
+/*
+LENGTH('KOREA')
+---------------
+5
+*/
+
+--실습 5)
+SELECT 'LeeKwonWoo'
+  FROM dual
+;
