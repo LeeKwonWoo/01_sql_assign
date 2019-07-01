6e65940a100a3e9f81e1a68dec08ebacecaae398
LeeKwonWoo <kenu12@Naver.com>
2019-07-01 16:17:49 +0900

    과제추가 10마무리

--- 이권우10.sql
+++ 이권우10.sql
@@ -104,12 +104,12 @@ ALTER TABLE salseman MODIFY userid VARCHAR2(30);
    ----------------------------------------
     
     GAME
-    GAME_CODE NUMBER(2)     PRIMAMRY KEY - 게임코드 10
-    GAME_NAME VARCHAR2(200) NOT NULL     - 게임이름
+    GAME_CODE NUMBER(2)     PRIMAMRY KEY -- 게임코드 10
+    GAME_NAME VARCHAR2(200) NOT NULL     -- 게임이름
     
     GMEMBER
-    ID         VARCHAR2(4)  PRIMAMRY KEY - 회원의 아이디  M001
-    NAME       VARCHAR2(15) NOT NULL     - 회원의 이름
+    ID         VARCHAR2(4)  PRIMAMRY KEY -- 회원의 아이디  M001
+    NAME       VARCHAR2(15) NOT NULL     -- 회원의 이름
     
     MEMBER_GAME_HISTORY
     ID         VARCHAR2(4)  FOREEIGN KEY 설정,이름 : FK_ID
@@ -117,4 +117,49 @@ ALTER TABLE salseman MODIFY userid VARCHAR2(30);
     YEAR       NUMBER(4)    게임을 한 년도
     GAME_CODE  NUMBER(2)    FK 설정 이름 : FK_GAME_CODE
                             GAME테이블의 GAME_CODE 컬럼을 참조하도록 설정
+*/
+
+DROP TABLE game;
+CREATE TABLE game
+( GAME_CODE NUMBER(2)     PRIMARY KEY -- 게임코드 10
+ ,GAME_NAME VARCHAR2(200) NOT NULL    -- 게임이름
+);
+
+INSERT INTO "SCOTT"."GAME" (GAME_CODE, GAME_NAME) VALUES ('10', 'LEAGUEOFLEGEND');
+INSERT INTO "SCOTT"."GAME" (GAME_CODE, GAME_NAME) VALUES ('20', 'BATTLEGROUND');
+INSERT INTO "SCOTT"."GAME" (GAME_CODE, GAME_NAME) VALUES ('30', 'CRAZYACADE');
+COMMIT;
+
+DROP TABLE gmember;
+CREATE TABLE gmember
+( ID         VARCHAR2(4)  PRIMARY KEY -- 회원의 아이디  M001
+ ,NAME       VARCHAR2(15) NOT NULL    -- 회원의 이름
+);
+
+INSERT INTO "SCOTT"."GMEMBER" (ID, NAME) VALUES ('M001', '이권우');
+INSERT INTO "SCOTT"."GMEMBER" (ID, NAME) VALUES ('M002', '홍길동');
+INSERT INTO "SCOTT"."GMEMBER" (ID, NAME) VALUES ('M003', '김광현');
+COMMIT;
+
+CREATE TABLE MEMBER_GAME_HISTORY
+( ID         VARCHAR2(4)  
+ ,YEAR       NUMBER(4)    
+ ,GAME_CODE  NUMBER(2)
+ , CONSTRAINT FK_ID        FOREIGN KEY(ID)        REFERENCES GMEMBER(ID)
+ , CONSTRAINT FK_GAME_CODE FOREIGN KEY(GAME_CODE) REFERENCES GAME(GAME_CODE)
+);
+
+INSERT INTO "SCOTT"."MEMBER_GAME_HISTORY" (ID, YEAR, GAME_CODE) VALUES ('M001', '2015', '30');
+INSERT INTO "SCOTT"."MEMBER_GAME_HISTORY" (ID, YEAR, GAME_CODE) VALUES ('M002', '2018', '10');
+INSERT INTO "SCOTT"."MEMBER_GAME_HISTORY" (ID, YEAR, GAME_CODE) VALUES ('M003', '2019', '20');
+
+SELECT *
+  FROM game g, gmember m, member_game_history h
+ WHERE m.id = 'M001' AND h.id = 'M001' AND g.game_code = h.game_code
+;
+
+/*
+GAME_CODE,  GAME_NAME,   ID,    NAME,   ID_1,   YEAR,   GAME_CODE_1
+-------------------------------------------------------------------
+30	        CRAZYACADE	M001	이권우	M001	2015	30
 */
